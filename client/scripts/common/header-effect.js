3ac206cb06382cf803f95b7eba4d856fcf930372
wangyanjun <linder0209@126.com>
2019-09-30 18:07:49 +0800

    优化调整

--- client/scripts/common/header-effect.js
+++ client/scripts/common/header-effect.js
@@ -2,24 +2,17 @@ import { addClass, removeClass, containsClass } from '../utils/dom-class';
 import { doc } from '../utils/perfect';
 
 export default function () {
-  window.addEventListener('wheel', (event) => {
+  window.addEventListener('scroll', (event) => {
     if (containsClass('body', 'header-menu-open')) {
       return;
     }
-    const { deltaX, deltaY } = event;
-    const detail = deltaX || deltaY;
-    const direction = (
-      detail < -1 ? 'up' : (
-        detail > 1 ? 'down' : ''
-      )
-    );
   
     const { scrollTop } = doc.documentElement;
     
-    if (direction === 'up') {
-      removeClass('header.header', 'hide');
-    } else if (direction === 'down' && scrollTop > 100) {
+    if (scrollTop > 100) {
       addClass('header.header', 'hide');
+    } else {
+      removeClass('header.header', 'hide');
     }
   }, false);
 };
